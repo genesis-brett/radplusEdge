cf8974b69d7f2d231f2e0d500ddfe9e5856a869d
genesis-brett <brett.l@gen-game.com>
2020-08-14 15:20:03 +0800

    add full lootbox and reward setting

--- clearCache.sh
+++ clearCache.sh
@@ -4,3 +4,5 @@ curl -X DELETE 'http://10.201.11.54:8090/api/genplus/levelup/levelupwagerexchang
 curl -X DELETE 'http://10.201.11.55:8090/api/genplus/levelup/levelupwagerexchange'
 curl -X PUT 'http://10.201.11.54:8090/api/genplus/levelupseason/clearcache'
 curl -X PUT 'http://10.201.11.55:8090/api/genplus/levelupseason/clearcache'
+curl -X PUT 'http://10.201.11.54:8090/api/genplus/leveluprewardroad/clearcache'
+curl -X PUT 'http://10.201.11.55:8090/api/genplus/leveluprewardroad/clearcache'
--- insertNewRewardRoad.sh
+++ insertNewRewardRoad.sh
@@ -7,14 +7,23 @@ do
   points=$(echo $rewardRoad | cut -d, -f2)
   radcoins=$(echo $rewardRoad | cut -d, -f3)
   boostRate=$(echo $rewardRoad | cut -d, -f4)
+  boostRateLevel=$(echo $rewardRoad | cut -d, -f5)
   if [[ $levelStr == "Milestone" ]]; then
      type=${levelStr^^}
      ((index=index+1))
      levelNumberStr=$levelNumber-$index
+     orderNumber=""
+     boostRateLevelStr="0"
   else
      type=$(echo "LEVEL")
      levelNumber=$(echo $levelStr | awk -F"vl" '{print $2}')
      levelNumberStr=$levelNumber
+     orderNumber=$(echo "\"order\"":$levelNumber",")
+     if [[ -z $boostRateLevel ]]; then
+        boostRateLevelStr="0"
+     else
+        boostRateLevelStr=$boostRateLevel
+     fi
      index=0
   fi
 
@@ -27,7 +36,8 @@ do
   else
       reward=' '
   fi
-  echo "radcoins:"$radcoins",reward:"$reward," boostRate:"$boostRate
+  
+  echo "boostRateLevel:"$boostRateLevelStr
   curl --location --request POST "http://$host:8090/api/genplus/leveluprewardroad" \
     --header 'Content-Type: application/json' \
     --data-raw "{
@@ -37,6 +47,8 @@ do
         \"rewards\": [ 
 		       $reward
         ],
+        $orderNumber
+        \"radCoinBoostLevel\": \"$boostRateLevelStr\",
         \"neededLevelUpPoints\": $points,
         \"rewardRoadType\": \"$type\"
     }"
