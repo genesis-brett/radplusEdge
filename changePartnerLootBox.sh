76b9035387deb281426d1c30580d2d1e6f37076b
genesis-brett <brett.l@gen-game.com>
2020-09-01 15:10:52 +0800

    improve changePartnerLootBox.sh

--- changePartnerLootBox.sh
+++ changePartnerLootBox.sh
@@ -1,23 +1,30 @@
-db.lootBoxPartnerConfig.update({partnerCode:partner},{$set:{active:false}},false,true);
-db.lootBoxPartnerConfig.insert([
-{
-	"active" : true,
-	"lootBoxId" : "5f27b9fd953487eaf5097b4d",
-	"partnerCode" : partner
-},
-{
-	"active" : true,
-	"lootBoxId" : "5f3603a5483df3968b8447d6",
-	"partnerCode" : partner
-},
-{
-	"active" : true,
-	"lootBoxId" : "5f3603a5483df3968b8447d7",
-	"partnerCode" : partner
-},
-{
-	"active" : true,
-	"lootBoxId" : "5f27b9fd953487eaf5097b4e",
-	"partnerCode" : partner
-}]);
-
+db.lootBoxPartnerConfig.distinct("partnerCode").forEach(partner => {
+    db.lootBoxPartnerConfig.update({
+        partnerCode: partner
+    }, {
+        $set: {
+            active: false
+        }
+    }, false, true);
+    db.lootBoxPartnerConfig.insert([{
+            "active": true,
+            "lootBoxId": "5f27b9fd953487eaf5097b4d",
+            "partnerCode": partner
+        },
+        {
+            "active": true,
+            "lootBoxId": "5f3603a5483df3968b8447d6",
+            "partnerCode": partner
+        },
+        {
+            "active": true,
+            "lootBoxId": "5f3603a5483df3968b8447d7",
+            "partnerCode": partner
+        },
+        {
+            "active": true,
+            "lootBoxId": "5f27b9fd953487eaf5097b4e",
+            "partnerCode": partner
+        }
+    ]);
+});
