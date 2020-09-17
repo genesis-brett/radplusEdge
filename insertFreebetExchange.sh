2346b7f4c7220225168987ac4970ae25c76dee53
genesis-brett <brett.l@gen-game.com>
2020-09-17 10:21:35 +0800

    change to using host variable

--- insertFreebetExchange.sh
+++ insertFreebetExchange.sh
@@ -1,10 +1,11 @@
 xfile=$1
+host=$2
 for exchangeRate in $(cat $1)
 do
   currency=$(echo $exchangeRate | cut -d, -f1)
   rate=$(echo $exchangeRate | cut -d, -f2)
   echo $rate 
-  curl --location --request POST 'http://10.201.11.54:8090/api/genplus/levelup/levelupwagerexchange' \
+  curl --location --request POST "http://$host:8090/api/genplus/levelup/levelupwagerexchange" \
     --header 'Content-Type: application/json' \
     --data-raw "{
 	\"exchangeType\":\"RADCOIN_FREE_BET\",
--- insertNewRadcoinExchange.sh
+++ insertNewRadcoinExchange.sh
@@ -1,10 +1,11 @@
 xfile=$1
+host=$2
 for exchangeRate in $(cat $1)
 do
   currency=$(echo $exchangeRate | cut -d, -f1)
   betAmount=$(echo $exchangeRate | cut -d, -f3)
   rate=$(echo $exchangeRate | cut -d, -f2)
-  curl --location --request POST 'http://10.201.11.54:8090/api/genplus/levelup/levelupwagerexchange' \
+  curl --location --request POST "http://$host:8090/api/genplus/levelup/levelupwagerexchange" \
     --header 'Content-Type: application/json' \
     --data-raw "{
 	\"exchangeType\":\"RADCOIN_EXCHANGE\",
