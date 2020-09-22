bb5db0392c1e0fe0e0b88a5dd900a50e5248d578
genesis-brett <brett.l@gen-game.com>
2020-09-22 12:28:06 +0800

    lootbox for bbin and language

--- insertNewLootBox.sh
+++ insertNewLootBox.sh
@@ -1,4 +1,6 @@
+db.lootBox.insert([
 {
+    "_id":ObjectId("5f27b9fd953487eaf5097b4d"),
 	"lootBoxName" : {
 		"en" : "Quick Box",
 		"zh-hant" : "入門寶箱",
@@ -11,8 +13,8 @@
 		"ja" : "Quick Box"
 	},
 	"lootBoxType" : "QUICK",
-	"createTime" : ISODate("2019-09-11T10:06:07.646+08:00"),
-	"accountId" : "Brett",
+	"createTime" : new ISODate(),
+	"accountId" : "edge",
 	"comments" : {
 		"en" : "Quick Try",
 		"zh-hant" : "體驗開箱小幸運",
@@ -25,21 +27,22 @@
 		"ja" : "クイックトライ"
 	},
 	"contentShorts" : {
-		"en" : "Boost your Radcoins",
-		"zh-hant" : "8R幣翻三倍的機會",
-		"zh-hans" : "8R币翻三倍的机会",
-		"vi" : "Tăng Radcoins của bạn",
-		"th" : "บูสต์ Radcoins ของคุณ",
-		"in" : "Tingkatkan Radcoins Anda",
-		"ru" : "Повысь свой Radcoins",
-		"tr" : "Radcoin Desteği",
-		"ja" : "Radcoinsをブースト"
+		"en" : "Boost your Reward Coins",
+		"zh-hant" : "獎勵幣翻倍的機會",
+		"zh-hans" : "奖励币翻倍的机会",
+		"vi" : "Tăng Xu Thưởng của bạn",
+		"th" : "บูสต์ เหรียญรางวัล ของคุณ",
+		"in" : "Tingkatkan Koin Hadiah\nAnda",
+		"ru" : "Повысь свой Монеты-награды",
+		"tr" : "Ödül Jetonları Desteği",
+		"ja" : "リワードコインをブースト"
 	},
 	"showOnTop" : false,
 	"price" : 8.0
 },
 
 {
+    "_id":ObjectId("5f27b9fd953487eaf5097b4e"),
 	"lootBoxName" : {
 		"en" : "Plus Box",
 		"zh-hant" : "淘金寶箱",
@@ -52,8 +55,8 @@
 		"ja" : "Plus Box"
 	},
 	"lootBoxType" : "PLUS",
-	"createTime" : ISODate("2019-09-11T10:07:07.769+08:00"),
-	"accountId" : "Brett",
+	"createTime" : new ISODate(),
+	"accountId" : "edge",
 	"comments" : {
 		"en" : "Most Popular",
 		"zh-hant" : "人氣寶箱,拚比手氣",
@@ -80,6 +83,7 @@
 	"price" : 88.0
 },
 {
+    "_id":ObjectId("5f3603a5483df3968b8447d6"),
 	"lootBoxName" : {
 		"en" : "Luxury Box",
 		"zh-hant" : "土豪寶箱",
@@ -92,8 +96,8 @@
 		"ja" : "Luxury Box"
 	},
 	"lootBoxType" : "LUXURY",
-	"createTime" : ISODate("2019-11-05T12:17:01.583+08:00"),
-	"accountId" : "Brett",
+	"createTime" : new ISODate(),
+	"accountId" : "edge",
 	"comments" : {
 		"en" : "High Reward",
 		"zh-hant" : "躋身土豪發發發",
@@ -120,6 +124,7 @@
 	"price" : 888.0
 },
 {
+    "_id":ObjectId("5f3603a5483df3968b8447d7"),
 	"lootBoxName" : {
 		"en" : "Jackpot Box",
 		"zh-hant" : "至尊寶箱",
@@ -132,8 +137,49 @@
 		"ja" : "Jackpot Box"
 	},
 	"lootBoxType" : "JACKPOT",
-	"createTime" : ISODate("2019-11-05T12:24:35.081+08:00"),
-	"accountId" : "Brett",
+	"createTime" : new ISODate(),
+	"accountId" : "edge",
+	"comments" : {
+		"en" : "Big Pay Day",
+		"zh-hant" : "獎金狂飆拿不停",
+		"zh-hans" : "奖金狂飙拿不停",
+		"vi" : "Ngày Trả Lớn",
+		"th" : "วันจ่ายก้อนโต",
+		"in" : "Hari Gajian Besar",
+		"ru" : "Высокая выплата",
+		"tr" : "Ödeme Günü",
+		"ja" : "高額支給"
+	},
+	"contentShorts" : {
+		"en" : "Win up to\n {value} {currency}",
+		"zh-hant" : "頭獎高達\n {value} {currency}",
+		"zh-hans" : "头奖高达\n {value} {currency}",
+		"vi" : "Thắng lên đến \n{value} {currency} ",
+		"th" : "รางวัลสูงสุดถึง \n{value} {currency}",
+		"in" : "Menangkan hingga \n {value} {currency}",
+		"ru" : "Выиграйте до\n{value} {currency}",
+		"tr" : "kazanma şansı\n{value} {currency}",
+		"ja" : "獲得: 最大 \n{value} {currency}"
+	},
+	"showOnTop" : false,
+	"price" : 8888.0
+},
+{
+    "_id" : ObjectId("5f697bf3a06636633d44da2d"),
+	"lootBoxName" : {
+		"en" : "Jackpot Box",
+		"zh-hant" : "至尊寶箱",
+		"zh-hans" : "至尊宝箱",
+		"vi" : "Jackpot Box",
+		"th" : "Jackpot Box",
+		"in" : "Jackpot Box",
+		"ru" : "Jackpot Box",
+		"tr" : "Jackpot Box",
+		"ja" : "Jackpot Box"
+	},
+	"lootBoxType" : "JACKPOT",
+	"createTime" : new ISODate(),
+	"accountId" : "edge-bbin",
 	"comments" : {
 		"en" : "Big Pay Day",
 		"zh-hant" : "獎金狂飆拿不停",
@@ -159,3 +205,4 @@
 	"showOnTop" : false,
 	"price" : 8888.0
 }
+]);
